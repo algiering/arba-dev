a670f02f3533233e170d4e372befd3f7e25f0eec
algiering <algiering@naver.com>
2017-09-22 18:32:11 +0900

    123

--- JAVA/Work/JavaApplication/src/main/java/test/game369.java
+++ JAVA/Work/JavaApplication/src/main/java/test/game369.java
@@ -4,20 +4,15 @@ public class game369 {
 
     public static void main(String[] args) {
         
-        int x = 0;
 
         for (int i = 1; i < 1000; i = i + 1) {
             
                         
-         if (0 == ((i % 10) % 3)) {
+         if ((0==((i % 10) % 3))&&((0!=i%10)&&(0!=i/3))) {
                 
-             if (0 >= ((i/10))%3)
-                
-                System.out.println(i);
+             System.out.println(i);
                 
             }
         }
     }
 }
-    
-
