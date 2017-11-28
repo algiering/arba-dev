238d1b3b5e27d9692a0fde804f3018382fa1dd49
algiering <algiering@naver.com>
2017-11-28 14:15:07 +0900

    234324

--- JAVA/Work/JavaApplication/src/main/java/test/game369.java
+++ JAVA/Work/JavaApplication/src/main/java/test/game369.java
@@ -3,17 +3,9 @@ package test;
 public class game369 {
 
     public static void main(String[] args) {
-        
 
         for (int i = 1; i < 1000; i = i + 1) {
-            
-            String x = String.valueOf(i);
-            
-            if(x.contains("3")||x.contains("6")||x.contains("9")){
-                
-            System.out.println(i+"박수 한번");
-                
-            }
+            if (i%10)
         }
     }
 }
